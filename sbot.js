b29457f72fbd42230af816f425dce5e95a3bfed6
Sean Burke <sean111@gmail.com>
2015-06-01 22:00:11 -0400

    Cleaned up a few things

--- sbot.js
+++ sbot.js
@@ -1,23 +1,6 @@
 $('.footercenter').append( "<span style='color: #FFF'>SBOT!</span>" );
 var player1 = null;
 var player2 = null;
-// betObserver = new MutationObserver( function( mutation ) {
-//     console.log( mutation );
-//     if( $('#betstatus').html() == "Bets are OPEN!") {
-//         var p1 = $('#player1').val();
-//         var p2 = $('#player2').val();
-//         console.log( { player1: p1, player2: p2 } );
-//     }
-//     else {
-//         //Need to check for winner
-//     }
-// } );
-
-// config = {
-//     attributes: true
-// }
-
-// betObserver.observe( $('#betstatus')[0], config );
 
 $('#betstatus').watch( {
     properties: "prop_innerHTML",
@@ -31,15 +14,16 @@ $('#betstatus').watch( {
         }
         else if( text.indexOf( "Team Red" ) > 0 ) {
             postMatch( p1, p2, 1 );
-            console.log( "Player 1 Wins!" );
         }
         else if( text.indexOf( "Team Blue" ) > 0 ) {
             postMatch( p1, p2, 2 );
-            console.log( "Player 2 Wins!" );
         }
     }
 } );
 
 function postMatch( p1, p2, winner ) {
-
+    //Make sure that the players were scraped before sending data to the server
+    if( p1.length > 1 && p2.length> 2 ) {
+        console.log( 'Player ' + winner + ' Wins!' );
+    }
 }
